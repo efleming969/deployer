0536f907f8237aa1aa4649a59dd10abcaba78e66
Erick Fleming <efleming969@gmail.com>
2016-06-04 07:38:46 -0500

    switched to using ES6 templates for MainView

--- src/server/MainView.js
+++ src/server/MainView.js
@@ -1,39 +1,40 @@
 var head = function()
 {
-  return (
-    '<head>' +
-      '<title>Deployer</title>' +
+  return `
+    <head>
+      <title>Deployer</title>
 
-      '<meta charset="utf-8">' + 
-      '<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">' + 
-      '<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">' + 
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-      '<link rel="shortcut icon" href="/favicon.ico?v=1">' + 
+      <link rel="shortcut icon" href="/favicon.ico?v=1">
 
-      '<script src="/index.js"></script>' + 
+      <script src="/node_modules/ramda/dist/ramda.js"></script>
+      <script src="/src/client/Main.js"></script>
 
-    '</head>'
-  )
+    </head>
+  `
 }
 
 var body = function()
 {
-  return (
-    '<body>' +
+  return `
+    <body>
 
-      '<main id="main" role="main">' +
-        '<header>' +
-          '<progress max="100" value="0" style="width:100%"></progress>' +
-        '</header>' +
-        '<section><h1>deployer:1.0.0</h1></section> ' +
-        '<footer></footer>' +
-      '</main>' +
+      <main id="main" role="main">
+        <header>
+          <progress max="100" value="0" style="width:100%"></progress>
+        </header>
+        <section><h1>deployer:1.0.0</h1></section>
+        <footer></footer>
+      </main>
 
-      '<script>' +
-        'document.addEventListener( "DOMContentLoaded", Main.run )' +
-      '</script>' +
-    '</body>'
-  )
+      <script>
+        document.addEventListener( "DOMContentLoaded", Main.run )
+      </script>
+    </body>
+  `
 }
 
 exports.renderer = function( configData )
