fbda30b944c16fcdb55a64f51ee10279ae19c5e4
Erick Fleming <efleming969@gmail.com>
2016-06-04 08:34:59 -0500

    removed useless github webhook test script

--- misc/test.sh
+++
@@ -1,9 +0,0 @@
-curl -i \
-  -H 'Content-Type: application/json' \
-  -H 'X-GitHub-Event: push' \
-  -H 'X-GitHub-Delivery: bc2cf580-2142-11e6-8342-6f007b3dd33f' \
-  -H 'X-Hub-Signature: sha1=33ad397d5d0ea5762fa0d449d548b064173d8faf' \
-  -X POST \
-  -d '{"ref":"refs/heads/master", "repository": { "name": "webowler" } }' \
-  http://localhost:9000/webhook
-
