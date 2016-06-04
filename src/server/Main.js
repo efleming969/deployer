7c478667359e63ba1badbb22f7980aeeaf9883bd
Erick Fleming <efleming969@gmail.com>
2016-06-03 21:34:07 -0500

    added bundler and played with styling

--- src/server/Main.js
+++ src/server/Main.js
@@ -3,68 +3,55 @@ var HTTP = require( 'http' )
 var Express = require( 'express' )
 var BodyParser = require( 'body-parser' )
 var ChildProcess = require( 'child_process' )
-var RF = require( 'ramda-fantasy' )
-var HTTPProxy = require( 'http-proxy' )
-
-var Success = RF.Either.Left
-var Failure = RF.Either.Right
 
 var Utils = require( './Utils' )
+var Bundler = require( './Bundler' )
+var MainView = require( './MainView' )
 
 var secret = process.env.GITHUB_WEBHOOK_SECRET
 var app = Express()
-var proxy = HTTPProxy.createProxyServer()
 
-var verifyGithubSignature = BodyParser.json(
-  { verify: function( req, res, buffer )
+var jsonVerifier = function( req, res, buffer )
+{
+  var githubProps = Utils.extractGithubProps( req.headers )
+
+  if ( githubProps.isLeft )
+    throw new Error( githubProps.value )
+  else if ( githubProps.value.xHubSignature !== Utils.signBlob( secret, buffer ) )
+    throw new Error( 'Not valid github signature' )
+}
+
+var respondToGithub = function( req, res )
+{
+  console.log( 'all is good' )
+  res.send( { "ok": true } )
+}
+
+var kickOffDeploymentScript = function( req, res )
+{
+  var process = ChildProcess.spawn('echo', ['hello']);
+
+  res.statusCode = 200
+  res.write( 'started process\n' )
+
+  process.stdout.on(
+    'data'
+  , function( data )
     {
-      var githubProps = Utils.extractGithubProps( req.headers )
-
-      if ( githubProps.isLeft )
-        throw new Error( githubProps.value )
-      else if ( githubProps.value.xHubSignature !== Utils.signBlob( secret, buffer ) )
-        throw new Error( 'Not valid github signature' )
+      res.end( 'received: ' + data.toString() )
     }
-  }
-)
+  )
+}
 
-app.get(
-  '/'
-, function( req, res )
-  {
-    proxy.web( req, res, { target: 'http://webowler:8080' } )
-    //res.send( '<h1>Deployer</h1>' )
-  }
-)
+app.get( '/index.js' , Bundler( 'src/client/Main.js' ) )
+app.get( '/' , MainView.renderer() )
 
-app.post(
-  "/webhook"
-, verifyGithubSignature
-, function( req, res )
-  {
-    console.log( 'all is good' )
-    res.send( { "ok": true } )
-  }
-)
+app.post( '/webhook'
+  , BodyParser.json( { verify: jsonVerifier } )
+  , respondToGithub
+  )
 
-app.post(
-  "/webhook-test"
-, function( req, res )
-  {
-    var process = ChildProcess.spawn('echo', ['hello']);
-
-    res.statusCode = 200
-    res.write( 'started process\n' )
-
-    process.stdout.on(
-      'data'
-    , function( data )
-      {
-        res.end( 'received: ' + data.toString() )
-      }
-    )
-  }
-)
+app.post( '/webhook-test' , kickOffDeploymentScript )
 
 HTTP.createServer( app ).listen( 8080 )
 
---
+++ src/server/MainView.js
@@ -0,0 +1,47 @@
+var head = function()
+{
+  return (
+    '<head>' +
+      '<title>Deployer</title>' +
+
+      '<meta charset="utf-8">' + 
+      '<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">' + 
+      '<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">' + 
+
+      '<link rel="shortcut icon" href="/favicon.ico?v=1">' + 
+
+      '<script src="/index.js"></script>' + 
+
+    '</head>'
+  )
+}
+
+var body = function()
+{
+  return (
+    '<body>' +
+
+      '<main id="main" role="main">' +
+        '<header>' +
+          '<progress max="100" value="0" style="width:100%"></progress>' +
+        '</header>' +
+        '<section><h1>deployer:1.0.0</h1></section> ' +
+        '<footer></footer>' +
+      '</main>' +
+
+      '<script>' +
+        'document.addEventListener( "DOMContentLoaded", Main.run )' +
+      '</script>' +
+    '</body>'
+  )
+}
+
+exports.renderer = function( configData )
+{
+  return function( req, res )
+  {
+    res.set( 'Content-Type', 'text/html' )
+    res.send( '<!doctype html>' + head() + body() )
+  }
+}
+
