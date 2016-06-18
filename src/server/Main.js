21fe614576fe039eb895234c849777e7ccbf83d1
Erick Fleming <efleming969@gmail.com>
2016-06-18 09:16:25 -0500

    removed github webhook functionality

--- src/server/Main.js
+++ src/server/Main.js
@@ -9,37 +9,10 @@ var Docker = require( 'dockerode' )
 var Utils = require( './Utils' )
 var MainView = require( './MainView' )
 
-var secret = process.env.GITHUB_WEBHOOK_SECRET
 var app = Express()
 
-var jsonVerifier = function( req, res, buffer ) {
-  var githubProps = Utils.extractGithubProps( req.headers )
+var startDockerContainer = function( callback ) {
 
-  if ( githubProps.isLeft )
-    throw new Error( githubProps.value )
-  else if ( githubProps.value.xHubSignature !== Utils.signBlob( secret, buffer ) )
-    throw new Error( 'Not valid github signature' )
-}
-
-var respondToGithub = function( req, res ) {
-  console.log( 'all is good' )
-  res.send( { "ok": true } )
-}
-
-var kickOffDeploymentScript = function( req, res ) {
-  var process = ChildProcess.spawn('echo', ['hello']);
-
-  res.statusCode = 200
-  res.write( 'started process\n' )
-
-  process.stdout.on( 'data' , function( data ) {
-    res.end( 'received: ' + data.toString() )
-  } )
-}
-
-app.use( Express.static( '.' ) )
-
-app.post( '/commands/:id', function( req, res ) {
   var docker = new Docker( { protocol:'http', host: '127.0.0.1', port: 4243 } )
 
   var containerOptions = {
@@ -55,6 +28,12 @@ app.post( '/commands/:id', function( req, res ) {
         res.send( data )
     } )
   } )
+}
+
+app.use( Express.static( '.' ) )
+
+app.post( '/commands/:id', function( req, res ) {
+  res.send( 'id:' + req.params.id )
 } )
 
 app.get( '/events', function( req, res ) {
@@ -63,9 +42,6 @@ app.get( '/events', function( req, res ) {
 
 app.get( '/' , MainView.renderer() )
 app.get( '/index.js' , Bundler.create( 'src/client/Main.js' ) )
-
-app.post( '/webhook', BodyParser.json( { verify: jsonVerifier } ), respondToGithub )
-app.post( '/webhook-test' , kickOffDeploymentScript )
 
 HTTP.createServer( app ).listen( 8080 )
 
--- src/server/UnitTests.js
+++ src/server/UnitTests.js
@@ -16,42 +16,7 @@ var logger = {
 
 var whenDone = TestCore.run( logger )( 'UnitTests', {
 
-  'extractGithubProps': function( when ) {
-      var Utils = require( './Utils' )
-
-      when( {
-        'extracting incomplete properties': function( then ) {
-            var obj = {
-                'x-github-event': '1'
-              , 'x-github-delivery': '2'
-              }
-
-            var rst = Utils.extractGithubProps( obj )
-
-            then( { 'should return an error message':
-              [ rst.toString() , 'Either.Left("x-hub-signature is required")' ] 
-            } )
-          }
-        , 'extracting complete properties': function( then ) {
-            var obj = {
-                'x-hub-signature': '1'
-              , 'x-github-event': '2'
-              , 'x-github-delivery': '3'
-              }
-
-            var rst = Utils.extractGithubProps( obj )
-
-            then( { 'should return the properties':
-              [ rst.value
-              , { xHubSignature: '1', xGithubEvent: '2', xGithubDelivery: '3' }
-              ]
-            } )
-          }
-      } )
-
-  }
-
-, 'deployments': function( when ) {
+  'deployments': function( when ) {
 
     var Deployments = require( './Deployments' )
     var createDeploymentsFrom = R.reduce( Deployments.handle, Deployments.init() )
