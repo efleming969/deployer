e3706c1c379ac849dcf5d6f7e0b04ed4724dacdd
Erick Fleming <efleming969@gmail.com>
2016-07-04 11:49:02 -0400

    moved docker sample code to sep file

---
+++ src/server/Docker.js
@@ -0,0 +1,19 @@
+// var startDockerContainer = function( callback ) {
+
+//   var docker = new Docker( { protocol:'http', host: '127.0.0.1', port: 4243 } )
+
+//   var containerOptions = {
+//     Image: 'sudogs/webowler:1.4.5',
+//     PortBindings: { '8080/tcp': [ { HostPort: '8081' } ] },
+//   }
+
+//   docker.createContainer( containerOptions, function( error, container ) {
+//     container.start( function( error, data ) {
+//       if ( error )
+//         res.send( 'oops' + error.toString() )
+//       else
+//         res.send( data )
+//     } )
+//   } )
+// }
+
