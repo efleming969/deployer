e2c142ba64b9ca9c3867127831938092c4a0eebc
Erick Fleming <efleming969@gmail.com>
2016-07-04 15:59:22 -0400

    started an install script for ubuntu deployment
    setting up an instance on ubuntu needs a few steps and this script documents those steps until I can understand how to automate the whole process

---
+++ bin/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo 'this script does not do anything currently.  follow the steps manually'
+
+# apt update
+# apt upgrade
+# curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+# apt install -y docker.io nodejs
+# git clone https://github.com/efleming969/deployer.git
+# systemctl enable /home/erick/deployer/bin/deployer.service
+
