8b2a6950acb6b6eab445eefb202e073c02d2b346
Erick Fleming <efleming969@gmail.com>
2016-07-04 11:49:54 -0400

    rough take on the 'release' api service

--- src/server/Release.js
+++ src/server/Release.js
@@ -1,26 +1,15 @@
 var R = require( 'ramda' )
 
-exports.init = function() {
-  return []
-}
-
 exports.handle = function( state, event ) {
-  switch( event.eventType ) {
-    case 'DeploymentAdded':
-      return R.append( { id: event.data.id }, state )
+  switch( event.type ) {
+    case 'DeploymentCreated':
+      return { id: event.data.id, isComplete: false }
     default:
       return state
   }
 }
 
-exports.add = function( state, data ) {
-  return [ { eventType: 'DeploymentAdded', data: data } ]
-}
-
-exports.complete = function( state, id ) {
-  if ( R.find( R.propEq( 'id', id ), state ) )
-    return [ { eventType: 'DeploymentCompleted', data: { id: id } } ]
-  else
-    return []
+exports.create = function( state, id ) {
+  return [ { type: 'DeploymentCreated' , data: { id: id } } ]
 }
 
