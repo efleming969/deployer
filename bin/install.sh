1471bf5de82580c7bb247ce893255b558c6f2afe
Erick Fleming <efleming969@gmail.com>
2016-07-04 17:28:16 -0400

    remove letsencrypt and proxy experiments
    going to use nginx server going forward, much easier to setup and configure with letsencrypt certonly option
    instructions are in the install script

--- bin/install.sh
+++ bin/install.sh
@@ -4,8 +4,24 @@ echo 'this script does not do anything currently.  follow the steps manually'
 
 # apt update
 # apt upgrade
+
+# install and setup nginx with tls certificates
+########################################################################################
+# apt install letsencrypt
+# letsencrypt certonly
+# apt install nginx
+#
+# configure nginx to new letsencrypt certificate
+# listen 443 ssl default_server;
+# listen [::]:443 ssl default_server;
+
+# ssl_certificate /etc/letsencrypt/live/sudogs.com/fullchain.pem;
+# ssl_certificate_key /etc/letsencrypt/live/sudogs.com/privkey.pem;
+
+# node js and deployer
+########################################################################################
 # curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-# apt install -y docker.io nodejs
+# apt install -y build-essential docker.io nodejs
 # git clone https://github.com/efleming969/deployer.git
 # systemctl enable /home/erick/deployer/bin/deployer.service
 
--- src/server/Main.js
+++ src/server/Main.js
@@ -1,7 +1,6 @@
 var R = require( 'ramda' )
 var HTTP = require( 'http' )
 var Express = require( 'express' )
-var Rocky = require( 'rocky' )
 var BodyParser = require( 'body-parser' )
 var ChildProcess = require( 'child_process' )
 var Bundler = require( 'lymph-bundler' )
@@ -9,16 +8,9 @@ var Docker = require( 'dockerode' )
 
 var Utils = require( './Utils' )
 var MainView = require( './MainView' )
-var Bowling = require( './Bowling' )
 
-var proxy = Rocky()
 var app = Express()
 
-proxy
-  .get( '/apps/bowling' )
-  .forward( 'http://localhost:8081' )
-
-app.use( proxy.middleware() )
 app.use( Express.static( 'static' ) )
 
 // app.post( '/applications/releases', function( req, res ) {
@@ -35,35 +27,8 @@ app.get( '/events', function( req, res ) {
 app.get( '/', MainView.renderer() )
 app.get( '/index.js', Bundler.create( 'src/client/Main.js' ) )
 
-HTTP.createServer( app ).listen( 8080 )
-
-// // Note: using staging server url, remove .testing( ) for production
-// var LEX = require( 'letsencrypt-express' ).testing( )
-// // var LEX = require( 'letsencrypt-express' )
-
 // app.use( '/', function ( req, res ) {
 //   res.send( { success: true } )
 // } )
 
-// lexServer = LEX.create( {
-//   // ~/letsencrypt, /etc/letsencrypt, whatever you want
-//   configDir: './letsencrypt'
-//   // your express app (or plain node http app )
-// , onRequest: app
-//   // you can provide you own instance of letsencrypt if you need to configure it (with an agreeToTerms callback, for example )
-// , letsencrypt: null
-//   // PRODUCTION MODE needs this function, but only if you want automatic registration (usually not necessary) renewals for registered domains will still be automatic
-// , approveRegistration: function ( hostname, cb ) {  
-//     cb( null, {
-//       domains: [hostname]
-//     , email: 'erick@sudogs.com'
-//     , agreeTos: true
-//     } )
-//   }
-// } )
-
-// lexServer.listen( [80], [443, 5001], function () {
-//   console.log( "ENCRYPT __ALL__ THE DOMAINS!" )
-// } )
-
-Express().use( Bowling.create() ).listen( 8081  )
+HTTP.createServer( app ).listen( 8080 )
